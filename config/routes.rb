e246e28481b858b5fe2cb7926ed8f9328d47413a
Saki-htr <saki.hattori0116@gmail.com>
2024-12-30 22:36:33 +0900

    add: Rails側で、指定したLINE IDに一斉に出欠連絡するボタンを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'line_bot/callback'
+  get '/admin' => 'admin#index'
+  post '/callback' => 'line_bot#callback'
+  post '/send_attendance_check' => 'line_bot#send_attendance_check'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-  post '/callback' => 'line_bot#callback'
 end
